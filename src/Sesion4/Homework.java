c1c4f6c7bba0fb72ee1c7420ae5606ea3fd5b949
Angelo2918 <angelosima@icloud.com>
2024-08-22 17:07:25 +0100

    Homeworks

---
+++ src/Sesion4/Homework.java
@@ -0,0 +1,15 @@
+package Sesion4;
+
+public class Homework {
+    public static void main(String [] args) {
+       //Number 1
+            String stringOne = "OpenAI";
+            String stringTwo = "OpenAI";
+
+            // Check if they refer to the same object
+            boolean areSameObject = (stringOne == stringTwo);
+
+            System.out.println("Are stringOne and stringTwo the same object? " + areSameObject);
+    }
+}
+
---
+++ src/Sesion6JavaAPIs/Main.java
@@ -0,0 +1,7 @@
+package Sesion6JavaAPIs;
+
+public class Main {
+    public static void main(String[] args ){
+        System.out.println("Viva la figa!");
+    }
+}
