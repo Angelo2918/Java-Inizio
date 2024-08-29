57714289bfd789c80e1475cc46112f01940e4120
Angelo2918 <angelosima@icloud.com>
2024-08-29 20:45:54 +0100

    Remakes

--- src/Sesion_8_Arrays_and__ArrayList/ArrayListVsLinkedList.java
+++ src/Sesion_8_Arrays_and__ArrayList/ArrayListVsLinkedList.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ArrayListVsLinkedList {
     public static void main(String... args) {
-        System.out.println("Array exe");
+
         List<String> arrayList = new ArrayList<>();
         arrayList.add("Sam");
         arrayList.add("Ion");
