7dc38b0c4e0507336c9cc3cf71e8a0b58327c286
Angelo2918 <angelosima@icloud.com>
2024-09-02 21:30:45 +0300

    Homeworks

--- src/Recap/Main.java
+++ src/Recap/Main.java
@@ -1,48 +1,45 @@
 package Recap;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\nRecapitulare a sesiunilor anterioare, 1-8!==");
+      System.out.println("\nRecapitulare a sesiunilor anterioare, 1-8!==");
 
-        //EX..1
-        int x = 5;
-        System.out.println(x++ + ++x);//12rez 5 + 7
-        //EX..2
-        String str = "Hello";
-        str.concat(" World");//
-        System.out.println(str);//Just Hello
-        //EX..3
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers[5]);//array indexoutofboundexception
-        //EX..4
-        LocalTime time = LocalTime.of(23, 59);
-        System.out.println(time.plusMinutes(1));//00
-        //EX..5
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(1, 3);
-        System.out.println(list);//1,3,2
-        //EX..6
-        int x = 5;
-        System.out.println(x > 3 ? x < 4 ? "A" : "B" : "C");//b
-        //EX..7
-        int[] arr = {1, 2, 3, 4, 5};
-        for (int i = 0; i < arr.length - 2; i += 2) {
-            System.out.println(arr[i] + " ");////1-3rez
-            //EX..8
-            String s = "1";
-            s += 2;
-            s += 3.0;
+//        //EX..1
+//        int x = 5;
+//        System.out.println(x++ + ++x);//12rez 5 + 7
+//        //EX..2
+//        String str = "Hello";
+//        str.concat(" World");//
+//        System.out.println(str);//Just Hello
+//        //EX..3
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers[5]);//array indexoutofboundexception
+//        //EX..4
+//        LocalTime time = LocalTime.of(23, 59);
+//        System.out.println(time.plusMinutes(1));//00
+//        //EX..5
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(1, 3);
+//        System.out.println(list);//1,3,2
+//        //EX..6
+//        int x = 5;
+//        System.out.println(x > 3 ? x < 4 ? "A" : "B" : "C");//b
+//        //EX..7
+//        int[] arr = {1, 2, 3, 4, 5};
+//        for (int i = 0; i < arr.length - 2; i += 2) {
+//            System.out.println(arr[i] + " ");////1-3rez
+//            //EX..8
+//            String s = "1";
+//            s += 2;
+//            s += 3.0;
+//
+//            System.out.println(s);
+//            123.0
+//
+//
+      }
 
-            System.out.println(s);
-            123.0
-
-
-        }
-
-    }
+  }
