03bbbe92ae80512896d02a6c5d8f0ac35b311278
Angelo2918 <angelosima@icloud.com>
2024-09-11 09:22:24 +0100

    new

--- src/Sesion_11_JavaOpp_Inheritance_Encapsulation/Person.java
+++ src/Sesion_11_JavaOpp_Inheritance_Encapsulation/Person.java
@@ -2,9 +2,8 @@ package Sesion_11_JavaOpp_Inheritance_Encapsulation;
 
 public class Person {
     private String firstName;
-    private String LastName;
+    private String lastName;
     private int age;
-
 
     public String getFirstName() {
         return firstName;
@@ -14,29 +13,23 @@ public class Person {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-        if (age < 0)
-            System.out.println("Age cannot be nagative");
-        else {
-
+        if (age < 0) {
+            System.out.println("Age cannot be negative or 0");
+        } else {
             this.age = age;
         }
-
-        public String getLastName () {
-            return LastName;
-        }
-
-        public void setLastName (String LastName){
-            this.LastName = LastName;
-        }
     }
-
-    public void setLastName(String doe) {
-    }
-
-    public String getLastName() {
-    }
+}
