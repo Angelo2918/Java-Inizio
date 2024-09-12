996e328d6c6c881ebb6d962c2d413cd94f6483e1
Angelo2918 <angelosima@icloud.com>
2024-09-12 14:28:03 +0300

    Homework+

---
+++ src/Studi/probs.java
@@ -0,0 +1,20 @@
+package Studi;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class probs {
+    public static void main(String[] args) {
+        //System.out.println("Incercari de depasire a cunostintei! ");
+    tim();
+    }
+    public static void tim(){
+        Date curentDate = new Date();
+        System.out.println("Date " + curentDate );
+
+
+    }
+
+}
