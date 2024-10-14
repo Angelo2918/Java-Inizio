f5533d0a4efc016fc26cb1ee871b42824d1075d5
Angelo2918 <angelosima@icloud.com>
2024-10-14 17:50:21 +0300

    New

---
+++ src/Session_18_JavaStreams/Main.java
@@ -0,0 +1,7 @@
+package Session_18_JavaStreams;
+
+public class Main {
+    public static void main (String[] args){
+
+    }
+}
---
+++ src/Session_18_JavaStreams/Person.java
@@ -0,0 +1,32 @@
+package Session_18_JavaStreams;
+
+import java.util.List;
+
+public class Person {
+    private String name;
+    private int age;
+    private List<String> hobbies;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, List<String> hobbies) {
+        this.name = name;
+        this.hobbies = hobbies;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+}
