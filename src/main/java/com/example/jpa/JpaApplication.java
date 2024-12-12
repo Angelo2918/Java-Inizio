9e849682c4d5dcbfbdb44f5685bbd0ffca3b1f1c
Angelo2918 <angelosima@icloud.com>
2024-12-12 20:34:31 +0200

    aaa

--- src/main/java/com/example/jpa/JpaApplication.java
+++ src/main/java/com/example/jpa/JpaApplication.java
@@ -3,11 +3,9 @@ package com.example.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableWebSecurity
 public class JpaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/jpa/services/UserService.java
+++ src/main/java/com/example/jpa/services/UserService.java
@@ -1,8 +1,10 @@
 package com.example.jpa.services;
 import com.example.jpa.dtos.CreateUserDto;
+import com.example.jpa.models.Instrument;
 import com.example.jpa.models.User;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserService {
     Iterable<User> saveAll(List<User> users);
@@ -16,4 +18,6 @@ public interface UserService {
     User addUser(User user);
 
     User createUserFromDto(CreateUserDto userDto);
+
+    Set<Instrument> getWishlist(Long id);
 }
